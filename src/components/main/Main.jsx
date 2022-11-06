46fb35dfecefdd17a085fb9d9a4ad90cd7171090
Lautaro Asat <lautaronorielasat@gmail.com>
2022-11-06 16:24:29 -0300

    Add counter

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,19 +1,28 @@
+import { useEffect, useState } from "preact/hooks";
 import { IoCalendar } from "react-icons/io5";
 import { IoLocationOutline } from "react-icons/io5";
+import { useTiping } from "../../hooks/useTiping";
 import Counter from "./Counter";
 import Diary from "./Diary";
 import Speakers from "./Speakers";
 import Sponsors from "./Sponsors";
 
 const Main = () => {
+  const keywords = useTiping(
+    ["INNOVACIÓN", "TECNOLOGIA", "HABILIDADES BLANDAS"],
+    300
+  );
+
   return (
     <main className="text-center pt-10 font-inter desktop:pt-32 text-[var(--secondary-color)]  ">
       <section id="inicio" className="pb-10">
-        <h1 className="text-5xl desktop:text-8xl font-poppins font-bold pb-4">
-          #epetConf
+        <h1 className="text-center select-none text-5xl desktop:text-8xl font-poppins font-bold pb-4">
+          <span>#EpetConf</span>
         </h1>
-        <p className="text-sm desktop:text-lg font-inter text-[#f1f1f1] font-medium">
-          CONFERENCIA DE INNOVACION Y TECNOLOGIA
+        <p className="select-none text-sm desktop:text-lg font-inter text-[#f1f1f1] font-medium ">
+          <span className="border-r-2 border-white">
+            CONFERENCIA DE {keywords}
+          </span>
         </p>
         <section className="mt-8 flex justify-center text-[#121f3d]">
           <div className="py-4  px-4 ">
