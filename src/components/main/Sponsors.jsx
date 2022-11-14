11e54a1aac590d2cd2032eecc77b5e942a7e04b5
Lautaro Asat <lautaronorielasat@gmail.com>
2022-11-14 19:18:18 -0300

    Delete sponsor

--- src/components/main/Sponsors.jsx
+++ src/components/main/Sponsors.jsx
@@ -1,12 +1,10 @@
 import Subtitle from "../Subtitle";
-import sponsors from "../../db/sponsors.json";
 import Agrotoken from "../sponsors/Agrotoken";
 import Coderhouse from "../sponsors/Coderhouse";
 import Msconsultores from "../sponsors/Mconsultores";
 import ShellMartinez from "../sponsors/ShellMartinez";
 import ElIman from "../sponsors/ElIman";
 import Neucor from "../sponsors/Neucor";
-import Barbero from "../sponsors/Barbero";
 
 const Sponsors = () => {
   return (
@@ -16,7 +14,6 @@ const Sponsors = () => {
       <nav className="flex flex-col w-3/4 sm:w-2/4 lg:hidden gap-y-6 mx-auto">
         <ShellMartinez />
         <ElIman />
-        <Barbero />
         <p>Diesel Lange</p>
         <Msconsultores />
         <Coderhouse />
@@ -28,7 +25,6 @@ const Sponsors = () => {
       <nav className="hidden desktop:grid desktop:grid-cols-2 desktop:place-items-center mx-auto gap-y-6 w-3/5">
         <ShellMartinez />
         <ElIman />
-        <Barbero />
         <Msconsultores />
         <Agrotoken />
         <Coderhouse />
--- src/components/sponsors/Barbero.jsx
+++
@@ -1,7 +0,0 @@
-import barbero from "../../assets/sponsors/barbero.png";
-
-const Barbero = () => {
-  return <img src={barbero} />;
-};
-
-export default Barbero;
