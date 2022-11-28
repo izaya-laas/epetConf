a6f60a2f9e016c1386ca52a0ffc4f186a20cc71b
Lautaro Asat <lautaronorielasat@gmail.com>
2022-11-27 23:57:16 -0300

    Delete Confetti

--- src/components/main/Counter.jsx
+++ src/components/main/Counter.jsx
@@ -1,53 +1,33 @@
-import JSConfetti from "js-confetti";
-import { useEffect } from "preact/hooks";
 import { useRemainingTime } from "../../hooks/useRemainingTime";
-
-const jsConfetti = new JSConfetti();
 
 const startDate = new Date("November 23, 2022 08:45");
 
 const Counter = () => {
-	const { days, hours, minutes, seconds, isDone } = useRemainingTime(startDate);
+  const { days, hours, minutes, seconds } = useRemainingTime(startDate);
 
-	useEffect(() => {
-		if (!isDone) return;
-
-		const interval = setInterval(() => {
-			jsConfetti.addConfetti();
-		}, 450);
-
-		setTimeout(() => {
-			clearInterval(interval);
-		}, 10000);
-
-		setTimeout(() => {
-			jsConfetti.clearCanvas();
-		}, 15000);
-	}, [isDone]);
-
-	return (
-		<section>
-			<h2 className="text-xl">#epetConf comienza en:</h2>
-			<div className="flex gap-x-4 justify-center text-3xl   mt-4 text-[#121f3d]">
-				<div className="flex flex-col w-16">
-					<p className="">{days}</p>
-					<span className="text-sm">Días</span>
-				</div>
-				<div className="flex flex-col w-16">
-					<p>{hours}</p>
-					<span className="text-sm">Horas</span>
-				</div>
-				<div className="flex flex-col w-16">
-					<p>{minutes}</p>
-					<span className="text-sm">Minutos</span>
-				</div>
-				<div className="flex flex-col w-16">
-					<p>{seconds}</p>
-					<span className="text-sm">Segundos</span>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section>
+      <h2 className="text-xl">#epetConf comienza en:</h2>
+      <div className="flex gap-x-4 justify-center text-3xl   mt-4 text-[#121f3d]">
+        <div className="flex flex-col w-16">
+          <p className="">{days}</p>
+          <span className="text-sm">Días</span>
+        </div>
+        <div className="flex flex-col w-16">
+          <p>{hours}</p>
+          <span className="text-sm">Horas</span>
+        </div>
+        <div className="flex flex-col w-16">
+          <p>{minutes}</p>
+          <span className="text-sm">Minutos</span>
+        </div>
+        <div className="flex flex-col w-16">
+          <p>{seconds}</p>
+          <span className="text-sm">Segundos</span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Counter;
